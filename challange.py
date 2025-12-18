12ed0ce1ef094c34e9c4c45961a65c040d14c299
Iga <igakrsz@gmail.com>
2025-12-18 18:18:21 +0200

    Create challange.py

---
+++ challange.py
@@ -0,0 +1,23 @@
+# Challenge 1
+
+number = int(input("Enter a number: "))
+length = int(input("Enter the length: "))
+
+multiples = []
+
+for i in range(1, length + 1):
+    multiples.append(number * i)
+
+print(multiples)
+
+# Challenge 2
+
+word = input("Enter a word: ")
+
+result = ""
+
+for char in word:
+    if result == "" or char != result[-1]:
+        result += char
+
+print(result)
